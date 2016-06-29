a7b844d62ac78d9bd7fbe4ebc09e9b713d551db7
chenqi <qi.chen5@renren-inc.com>
2016-06-29 21:05:53 +0800

    vue resource

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,7 @@ var express = require('express'),
     db = require('./conn'); //先引入数据库链接
 
 router.get('/', function(req, resIndex) {
-  //db.pool.getConnection(function(err, connection) {
-  //  var sql = 'select * from test';
-  //  connection.query(sql, function(error, res) {
-  //      resIndex.render('index', {
-  //          title: "从这里开始",
-  //          res:res
-  //      });
-  //  });
-  //  connection.release();
-  //});
     resIndex.render('index', { });
-
 });
 
 module.exports = router;
